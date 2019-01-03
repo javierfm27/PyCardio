1a00beb6e8164f44d689fbf97eb04b77e55ebf0c
javierfm27 <javitofm27@github.com>
2019-01-03 10:34:39 +0100

    Package Created with HRV module

---
+++ PyCardio/__init__.py
@@ -0,0 +1,1 @@
+print("Se ha importado el paquete PyCardio")
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="PyCardio",
+    version="0.0.1",
+    author="Oscar Barquero",
+    author_email="oscar.barquero@urjc.es",
+    description="Package for heart analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/javierfm27/PyCardio",
+    packages=setuptools.find_packages(),
+    classifiers= [
+        "Programming Language :: Python :: 3 ",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Indepedent",
+        "Topic :: Scientific/Engineering :: Medical Science Apps",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ""
+    ]
+)
