72262fdff1d3fd8b13b49c1e1e9f5a71614e4f4d
javierfm27 <javitofm27@github.com>
2019-01-10 10:48:00 +0100

    CodeCov Test 5

---
+++ tests.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+from PyCardio.HRV.HRV import HRV
+
+class TestPycardio(unittest.TestCase):
+
+    def test_create_HRV(self):
+        x = HRV()
+        aseertIsInstance(HRV,HRV)
+
+if __name__ = "__main__":
+    unittest.main()
