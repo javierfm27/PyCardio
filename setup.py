003e8fa7f719d61fe13b03270d47bdcf0bdaeb44
javierfm27 <javitofm27@github.com>
2019-03-07 17:07:36 +0100

    setup.py classifier modified about the change of license

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="PyCardioTest",
-    version="0.0.6",
+    version="0.0.7",
     author="Oscar Barquero",
     author_email="oscar.barquero@urjc.es",
     install_requires= [
